fb813930f6c2d4791fd180f6f1e2f488be2c85dd
Taha Jadid <jadid.taha@waroom.com>
2023-09-26 15:05:45 +0100

    add result activity

--- app/src/main/java/com/example/dynamicforms/ResultActivity.kt
+++ app/src/main/java/com/example/dynamicforms/ResultActivity.kt
@@ -1,11 +1,26 @@
 package com.example.dynamicforms
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var results: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        results = findViewById(R.id.results)
+
+        initComponents()
+    }
+
+    /**
+     * function to init the content view
+     */
+    private fun initComponents() {
     }
 }
---
+++ app/src/main/java/com/example/dynamicforms/Results.kt
@@ -0,0 +1,6 @@
+package com.example.dynamicforms
+
+data class Results(
+    var id: String? = null,
+    var content: String? = null
+)
