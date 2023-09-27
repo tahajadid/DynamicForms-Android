a5782bbbf85a406225f7087c54e01b5c82bf4039
Taha Jadid <jadid.taha@waroom.com>
2023-09-27 10:29:45 +0100

    Add result list

--- app/src/main/java/com/example/dynamicforms/Sessions.kt
+++ app/src/main/java/com/example/dynamicforms/Sessions.kt
@@ -1,7 +1,11 @@
 package com.example.dynamicforms
 
 import com.example.dynamicforms.billsActivity.data.Merchants
+import com.example.dynamicforms.resultActivity.Results
 
 // list of all merchants
 internal var merchantSessionList = mutableListOf<Merchants>()
 internal var merchantLogo = ""
+
+// results
+internal var resultList = mutableListOf<Results>()
